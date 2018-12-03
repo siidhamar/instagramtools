b1566e90d690a96b37689227aca2053fa0f8e03c
siidhamar <32352907+siidhamar@users.noreply.github.com>
2018-12-03 16:33:01 +0700

    Update tools-fftauto.js

--- tools-fftauto.js
+++ tools-fftauto.js
@@ -218,8 +218,7 @@ const Excute = async function(User, TargetUsername, Text, Sleep, mysyntx){
 }
 console.log(chalk`{bold.green
   Ξ TITLE  : FFT *FOLLOW,LIKE,COMMENT TARGET FOLLOWER USER!
-  Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ RECODE : S I R E U M ( @siidhamar )
   Ξ STATUS : ITTYW : {bold.green Supported!}}
       `);
 inquirer.prompt(User)
