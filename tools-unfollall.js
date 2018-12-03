df2f2c2a5b1d7e4b3bb31160fd7cd4aef24f8d36
siidhamar <32352907+siidhamar@users.noreply.github.com>
2018-12-03 16:31:27 +0700

    Update tools-unfollall.js

--- tools-unfollall.js
+++ tools-unfollall.js
@@ -105,8 +105,7 @@ const Excute = async function(User,sleep,mysyntx){
 }
 console.log(chalk`{bold.green
   Ξ TITLE  : UNFALL *UNFOLOW ALL FOLLOWING INSTAGRAM!
-  Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ RECODE : S I R E U M ( @siidhamar )
   Ξ STATUS : ITTYW : {bold.green Supported!}}
       `);
 inquirer.prompt(User)
