d0b2cdd8af61f6f320e685f931dfc91288d762a1
siidhamar <32352907+siidhamar@users.noreply.github.com>
2018-12-03 16:33:31 +0700

    Update tools-fah.js

--- tools-fah.js
+++ tools-fah.js
@@ -163,8 +163,7 @@ const doMain = async (account, hastag, sleep, text, mysyntx) => {
 }
 console.log(chalk`{bold.green
   Ξ TITLE  : FAH *FOLLOW,LIKE,COMMENT TARGET HASTAG!
-  Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ RECODE : S I R E U M ( @siidhamar )
   Ξ STATUS : ITTYW : {bold.green Supported!}}
       `);
 inquirer.prompt(question)
